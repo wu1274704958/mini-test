bead0157b22ae064485eb043ab6541b153d6ed4e
wusj7856 <wusj@realbull.net>
2018-11-12 19:30:51 +0800

    test2_1

--- include/Test2.hpp
+++ include/Test2.hpp
@@ -5,9 +5,32 @@
 #include "TestFunc.hpp"
 namespace test2{
 
+
+template<int N, int... Indexes>
+struct IndexTuple {
+    using type = typename IndexTuple<N - 1,N - 1,Indexes...>::type;
+};// 3 -> 2 2 -> 1 1 2 -> 0 0 1 2 -> 0 1 2       
+
+template<int... indexes>
+struct IndexTuple<0, indexes...>
+{
+    typedef IndexTuple<indexes...> type;
+};
+template<int I>
+struct make_index{
+    using type = typename IndexTuple<I>::type;
+};
+
+template<int ...Argc>
+void print_index_tuple(IndexTuple<Argc ...> it)
+{
+    ((std::cout << Argc << " "),...); 
+}
+
 void test2_1()
 {
-    std::cout << "void test2_1()" << std::endl;
+    using type = make_index<7>::type;
+    print_index_tuple(type());
 }
 
 auto init()
