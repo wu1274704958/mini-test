f0a0286fe37f754e57a54456f647b2151a4eade0
wws <1274704958@qq.com>
2019-06-07 19:08:54 +0800

    .

--- include/lc1.hpp
+++ include/lc1.hpp
@@ -40,11 +40,38 @@ namespace lc1{
         dbg(removeDuplicates(in2));
         dbg(in2);
     }
-
+    int removeElement(std::vector<int>& nums, int val) {
+        if(nums.empty())
+            return 0;
+        int n = nums.size();
+        if(n == 1) return 1;
+        int i = 0,j = n -1;
+        while(i < j)
+        {
+            dbg(j);
+            dbg(i);
+            if(nums[i] == val)
+            {
+                if(nums[j] == val){ --j;continue;}
+                nums[i] = nums[j];
+                if(j + 1 >= i) return n -j;
+                --j;
+            }
+            ++i;
+        }
+        return n - j;
+    }
+    void test_removeElemens() {
+        std::vector<int> in1 = {3,2,2,3};
+        int res = removeElement(in1,3);
+        dbg(res);
+        dbg(in1);
+    }
     auto init()
     {
         return  wws::CreateTFArray(
-            CREATE_TEST_FUNC(test_removeDuplicates)
+            CREATE_TEST_FUNC(test_removeDuplicates),
+            CREATE_TEST_FUNC(test_removeElemens)
         );
     }
 }
