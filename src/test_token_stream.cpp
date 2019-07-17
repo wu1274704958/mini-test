0a3a67658755b4d684907070b390490b5f61784c
wws <1274704958@qq.com>
2019-07-17 16:15:32 +0800

    can transform line and point

--- src/test_token_stream.cpp
+++ src/test_token_stream.cpp
@@ -58,8 +58,13 @@ int main(int argc,char **argv)
 				std::cout << s << "\n";
 			}
 			confound_js(ts);
-			std::string out = "C:/Users/admin/Desktop/test2.js";
-			ts.save(out,true);
+			fs::path fn = f.filename();
+			std::string nn = fn.generic_string();
+			int i = nn.find_last_of('.');
+			nn.insert(i, 1, '2');
+			fs::path out = f.replace_filename(fs::path(nn));
+			std::string out_path = out.generic_string();
+			ts.save(out_path,true);
 		}
 	}
 	
@@ -84,30 +89,93 @@ std::string get_under_line(int n)
 template<typename T>
 void confound_js<T>(TokenStream<T>& ts)
 {
-	int line_n = 3;
+	int line_n = 3,l_line_n = 1;
 	std::unordered_map<std::string, int> g_map;
-	bool in_func = false;
+	std::unordered_map<std::string, int> l_map;
+	int if_stage = 0;
+	int deep = 0;
 	for (int i = 0;i < ts.tokens.size();++i)
 	{
 		auto& s = ts.tokens;
 		auto& curr = s[i];
+
+	
+		if (if_stage == 0 && curr.body == "function")
+			if_stage = 1;
+		
+		if (curr.per == '(' && if_stage == 1)
+		{
+			l_line_n = line_n + 1;
+			while (s[i].back != ')')
+			{
+				if (s[i].body != "")
+				{
+					l_map[s[i].body] = l_line_n;
+					s[i].body = get_under_line(l_line_n);
+					++l_line_n;
+				}
+				++i;
+			}
+			if (s[i].body != "" && s[i].back == ')')
+			{
+				l_map[s[i].body] = l_line_n;
+				s[i].body = get_under_line(l_line_n);
+				++l_line_n;
+			}
+			if_stage = 2;
+		}
+
+		if (curr.per == '{' && if_stage == 2)
+		{
+			if_stage = 3;
+			deep = 1;
+		}
+		if (curr.per == '{' && if_stage == 3)
+		{
+			++deep;
+		}
+
+		if (curr.back == '}' && if_stage == 3)
+		{
+			--deep;
+			if (deep == 0)
+			{
+				if_stage = 0;
+				l_map.clear();
+				l_line_n = 1;
+			}
+		}
+
 		if (curr.body == "var")
 		{
-			if (!in_func)
+			if (if_stage == 0)
 			{
 				g_map[s[i+1].body] = line_n;
 				++line_n;
 			}
 			else {
-
+				l_map[s[i + 1].body] = l_line_n;
+				++l_line_n;
 			}
 			continue;
 		}
 		for (auto it = g_map.begin(); it != g_map.end(); ++it)
 		{
-			if (it->first == curr.body)
+			if (it->first == curr.body && curr.per != '"' && curr.back != '"')
 			{
 				curr.body = get_under_line(it->second);
+			}
+		}
+
+		if (if_stage == 3)
+		{
+			auto & curr_local_map = l_map;
+			for (auto it = curr_local_map.begin(); it != curr_local_map.end(); ++it)
+			{
+				if (it->first == curr.body && curr.per != '"' && curr.back != '"')
+				{
+					curr.body = get_under_line(it->second);
+				}
 			}
 		}
 	}
