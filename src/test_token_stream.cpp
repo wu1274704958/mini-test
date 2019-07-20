d59d177a65868765d8fc867bbc4196737a12f531
wu1274704958 <1274704958@qq.com>
2019-07-20 12:51:18 +0800

    add confound as

--- src/test_token_stream.cpp
+++ src/test_token_stream.cpp
@@ -8,6 +8,8 @@ using namespace token;
 template<typename T>
 void confound_js(TokenStream<T>& ts);
 
+void confound_as(std::vector<Token>& ts);
+
 int main(int argc,char **argv)
 {
 	Token t;
@@ -28,6 +30,7 @@ int main(int argc,char **argv)
 		"}\n"
 		"\n"
 		"b(); ");
+		dbg("analyse");
 		ts.analyse();
 		for (auto &s : ts.tokens)
 		{
@@ -38,8 +41,20 @@ int main(int argc,char **argv)
 	{
 		namespace fs = std::filesystem;
 		fs::path f(argv[1]);
+		int is_support = 0;
+		if (f.has_extension())
+		{
+			auto ext = f.extension();
+			dbg(ext.generic_string());
+			if (ext == ".js" || ext == ".html")
+				is_support = 1;
+			if (ext == ".as")
+				is_support = 2;
+			dbg(is_support);
+		}
+		
 		//fs::path f("C:/Users/admin/Desktop/test.js");
-		if (fs::exists(f))
+		if (fs::exists(f) && is_support > 0)
 		{
 			auto path = f.generic_string();
 			dbg(path);
@@ -57,7 +72,18 @@ int main(int argc,char **argv)
 			{
 				std::cout << s << "\n";
 			}
-			confound_js(ts);
+			switch (is_support)
+			{
+			case 1:
+				confound_js(ts);
+				break;
+			case 2:
+				confound_as(ts.tokens);
+				break;
+			default:
+				break;
+			}
+
 			fs::path fn = f.filename();
 			std::string nn = fn.generic_string();
 			int i = nn.find_last_of('.');
@@ -169,6 +195,96 @@ void confound_js<T>(TokenStream<T>& ts)
 		if (if_stage == 3)
 		{
 			auto & curr_local_map = l_map;
+			for (auto it = curr_local_map.begin(); it != curr_local_map.end(); ++it)
+			{
+				if (it->first == curr.body && curr.per != '"' && curr.back != '"')
+				{
+					curr.body = get_under_line(it->second);
+				}
+			}
+		}
+	}
+}
+
+
+void confound_as(std::vector<Token>& ts)
+{
+	int line_n = 3, l_line_n = 1;
+	std::unordered_map<std::string, int> g_map;
+	std::unordered_map<std::string, int> l_map;
+	int if_stage = 0;
+	int deep = 0;
+	for (int i = 0; i < ts.size(); ++i)
+	{
+		auto& s = ts;
+		auto& curr = s[i];
+
+
+		if (if_stage == 0 && curr.body == "function")
+			if_stage = 1;
+
+		if (curr.per == '(' && if_stage == 1)
+		{
+			l_line_n = line_n + 1;
+			while (s[i].back != ')')
+			{
+				if (s[i].body != "" && s[i].back == ':')
+				{
+					l_map[s[i].body] = l_line_n;
+					s[i].body = get_under_line(l_line_n);
+					++l_line_n;
+				}
+				++i;
+			}
+			if_stage = 2;
+		}
+
+		if (curr.per == '{' && if_stage == 2)
+		{
+			if_stage = 3;
+		}
+		if (curr.per == '{' && if_stage == 3)
+		{
+			++deep;
+		}
+
+		if (curr.back == '}' && if_stage == 3)
+		{
+			--deep;
+			if (deep == 0)
+			{
+				if_stage = 0;
+				l_map.clear();
+				l_line_n = 1;
+			}
+		}
+
+		if (	(curr.body == "var" && s[i - 1].body == "private")
+			|| (curr.body == "var" && s[i - 1].body == "static" && s[i - 2].body == "private") 
+			)
+		{
+			if (if_stage == 0)
+			{
+				g_map[s[i + 1].body] = line_n;
+				++line_n;
+			}
+			else {
+				l_map[s[i + 1].body] = l_line_n;
+				++l_line_n;
+			}
+			continue;
+		}
+		for (auto it = g_map.begin(); it != g_map.end(); ++it)
+		{
+			if (it->first == curr.body && curr.per != '"' && curr.back != '"')
+			{
+				curr.body = get_under_line(it->second);
+			}
+		}
+
+		if (if_stage == 3)
+		{
+			auto& curr_local_map = l_map;
 			for (auto it = curr_local_map.begin(); it != curr_local_map.end(); ++it)
 			{
 				if (it->first == curr.body && curr.per != '"' && curr.back != '"')
