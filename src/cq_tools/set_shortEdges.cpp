a7af5918d2d10a7150f71bb228ead46d84948479
wws <1274704958@qq.com>
2019-11-18 15:13:14 +0800

    add get_pkg_name set_shortEdges .

--- src/cq_tools/set_shortEdges.cpp
+++ src/cq_tools/set_shortEdges.cpp
@@ -8,7 +8,8 @@ using namespace token;
 namespace fs = std::filesystem;
 
 void set_shortEdges(std::vector<Token>& ts);
-void set_shortEdges2(std::vector<Token>& ts);
+void set_shortEdges2(std::vector<Token>& ts, std::string& pkg_name);
+std::string get_pkg_name(std::vector<Token>& ts);
 
 int main(int argc, char** argv)
 {
@@ -18,7 +19,7 @@ int main(int argc, char** argv)
 		return -1;
 	}
 	fs::path f(argv[1]);
-	fs::path f2(argv[2]);
+	std::string s2(argv[2]);
 	//fs::path f2("C:\\Users\\admin\\Desktop\\qk_sub_pass\\out_pkg\\smali\\one\\chuanqi\\online\\AppEntry.smali");
 	//fs::path f("C:\\Users\\admin\\Desktop\\qk_sub_pass\\out_pkg\\res\\values\\styles.xml");
 	if (fs::exists(f))
@@ -41,6 +42,38 @@ int main(int argc, char** argv)
 		return -2;
 	}
 
+	std::string s3 = s2 + "AndroidManifest.xml";
+
+	fs::path mainfest(s3);
+	std::string pkg_name;
+	if (fs::exists(mainfest))
+	{
+		std::ifstream is(mainfest.generic_string(), std::ios::binary);
+		TokenStream<std::ifstream> ts(std::move(is));
+		ts.analyse();
+		
+		pkg_name = get_pkg_name(ts.tokens);
+		
+		for (auto& c : pkg_name)
+		{
+			if (c == '.')
+				c = '/';
+		}
+		dbg(pkg_name);
+	}
+	else
+	{
+		std::cout << "No find input file!" << std::endl;
+		return -2;
+	}
+
+	std::string s4 = s2 + "smali/";
+	s4 += pkg_name;
+	s4 += "/AppEntry.smali";
+
+	dbg(s4);
+
+	fs::path f2(s4);
 	if (fs::exists(f2))
 	{
 		std::ifstream is(f2.generic_string(), std::ios::binary);
@@ -50,7 +83,7 @@ int main(int argc, char** argv)
 		{
 			std::cout << t << std::endl;
 		}*/
-		set_shortEdges2(ts.tokens);
+		set_shortEdges2(ts.tokens,pkg_name);
 		auto out = f2.generic_string();
 		dbg(out);
 		ts.save(out, true);
@@ -103,7 +136,7 @@ void set_shortEdges(std::vector<Token>& ts)
 
 }
 
-void set_shortEdges2(std::vector<Token>& ts)
+void set_shortEdges2(std::vector<Token>& ts,std::string& pkg_name)
 {
 	bool in_onCreate = false;
 	auto it1 = -1;
@@ -141,6 +174,17 @@ void set_shortEdges2(std::vector<Token>& ts)
 			" move-result-object v2\n"
 			" invoke-virtual {v2, v0}, Landroid/view/Window;->setAttributes(Landroid/view/WindowManager$LayoutParams;)V\n"
 			, Token::None, '\n');
+
+		for (int i = 0; i < t.body.size(); ++i)
+		{
+			if (t.body[i] == 'L' && t.body[i + 1] == 'o' && t.body[i + 2] == 'n' && t.body[i + 3] == 'e')
+			{
+				t.body.replace(i + 1, 18, pkg_name);
+			}
+		}
+
+		dbg(t.body);
+
 		ts.insert(ts.begin() + it1, std::move(t));
 		++it1;
 		while (ts[it1].body != "invoke" && ts[it1 + 1].body != "super")
@@ -152,4 +196,34 @@ void set_shortEdges2(std::vector<Token>& ts)
 	}
 
 
+}
+
+std::string get_pkg_name(std::vector<Token>& ts)
+{
+	bool in_manifest = false;
+	bool catched = false;
+	std::string pkg_name;
+	for (int i = 0; i < ts.size(); ++i)
+	{
+		auto& t = ts[i];
+		if (t.body == "manifest" && t.per == '<')
+		{
+			in_manifest = true;
+		}
+		if (in_manifest && t.back == '>')
+		{
+			in_manifest = false;
+		}
+
+		if (in_manifest && t.body == "package" && t.back == '=')
+		{
+			if (ts[i + 1].per == '"' && ts[i + 1].back == '"')
+			{
+				pkg_name = ts[i + 1].body;
+				catched = true;
+				//dbg(pkg_name);
+			}
+		}
+	}
+	return pkg_name;
 }
